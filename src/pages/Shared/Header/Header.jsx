6811ecd0eadd50bde217bf08609a494a51f546eb
emamjion <emamjion@gmail.com>
2023-05-05 19:42:04 +0600

    create layout, header component and momentjs added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is Header</h2>
+            <div className='text-center my-5'>
+                <img src={logo} />
+                <h5 className='mt-4' style={{color: '#706F6F'}}>Journalism Without Fear or Favour</h5>
+                <p className='fs-5 fw-semibold' style={{color: '#706F6F'}}>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
         </div>
     );
 };
---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftNav = () => {
+    return (
+        <div>
+            <h4>Right Nav</h4>
+        </div>
+    );
+};
+
+export default LeftNav;
