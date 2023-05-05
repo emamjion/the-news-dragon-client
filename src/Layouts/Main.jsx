ddb41dfee34d8593d32bb0e7650297f528bb1318
emamjion <emamjion@gmail.com>
2023-05-05 18:24:27 +0600

    router setup finished and components created

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../pages/Shared/Header/Header';
+import Footer from '../pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header/>
+            
+            <Footer/>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'>copyright &copy; dragon news 2023</p>
+        </div>
+    );
+};
+
+export default Footer;
